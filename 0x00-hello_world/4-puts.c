7cd952eaa78e225e6850a4c541d6ce93f7de9315
Samson Hailu <alxsamsonhailu@gmail.com>
2021-08-29 16:26:54 +0300

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 /*
-/**
- * main - Entry point
- *
- * Description: prints "Programming is like building a multilingual puzzle"
+ Description: prints "Programming is like building a multilingual puzzle"
  */
 int main(void)
 {
