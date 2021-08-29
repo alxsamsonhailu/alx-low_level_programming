544c56be139978c3c8e315ef4b41402463d88c29
Samson Hailu <alxsamsonhailu@gmail.com>
2021-08-29 17:22:22 +0300

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,10 @@
  *
  * Description: prints the size of various types on the computer it is compiled
  * and run on
- * return 
+ *
+ * Return: Always 0 (Success)
  */
-int main(void)
+ int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
 	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
