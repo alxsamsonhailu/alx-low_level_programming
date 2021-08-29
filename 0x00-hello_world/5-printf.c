b4493db7781a00f4793e3ebccc7d30a98dc33d89
Samson Hailu <alxsamsonhailu@gmail.com>
2021-08-29 17:35:32 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ *
  * prints "with proper grammar, but the outcome is a piece of art,"
  * Description: prints "with proper grammar, but the outcome is a piece of
  * art,"
