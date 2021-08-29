cea3eb83e0f478a6cfde1ab70a4220742b0c81fa
Samson Hailu <alxsamsonhailu@gmail.com>
2021-08-29 16:07:28 +0300

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+/*Description: prints "Programming is like building a multilingual puzzle"
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
