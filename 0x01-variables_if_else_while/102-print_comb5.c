d7914f4b7101cf40228bd69824b71086a3daed48
Samson Hailu <alxsamsonhailu@gmail.com>
2021-08-30 12:33:40 +0300

    Print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,28 +1,44 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 /**
- * main - print pairs of double digit combos
- *
- * Description: print pairs of double digit combos
+ * main- Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j, k, m;
+	int i, j, k, l;
 
-	i = 48;
-	while (i < 58)
+	for (i = 48; i <= 57; i++)
 	{
-		j = 48;
-		m = 48;
-				k++;
+		for (j = 48; j <= 57; j++)
+		{
+			l = j + 1;
+			for (k = i; k <= 57; k++)
+			{
+				for (; l <= 57; l++)
+				{
+					putchar(i);
+					putchar(j);
+
+					putchar(32);
+
+					putchar(k);
+					putchar(l);
+
+					if (i < 57 || j < 56)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+				l = 48;
 			}
 		}
-
-			j++;
-		}
-		i++;
 	}
+
+	/* print a new line */
 	putchar(10);
 
 	return (0);
