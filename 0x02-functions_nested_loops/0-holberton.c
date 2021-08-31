44df2afacd9779113325c1bac7489f7a5d2c20e8
Samson Hailu <alxsamsonhailu@gmail.com>
2021-08-31 21:15:52 +0300

    update

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - prints Holberton
  *
@@ -7,12 +6,11 @@
  *
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 	char text[10] = "Holberton";
 	int i = 0;
-	
+
 	for (i = 0; i < 9; i++)
 	{
 		_putchar(text[i]);
