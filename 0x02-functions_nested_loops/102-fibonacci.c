7f278bf8cbf8bcb32b174b6a31cb899ec7a10c22
Samson Hailu <alxsamsonhailu@gmail.com>
2021-08-31 20:43:15 +0300

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	long i;
-	unsigned long n1, n2;
+	long int sum;
+	long int first, second;
+	int count;
+
+	sum = 0;
+	first = 0;
+	second = 1;
+	count = 0;
+
+	while (count < 49)
+	{
+		sum = first + second;
+		printf("%li, ", sum);
+		first = second;
+		second = sum;
+
+		count++;
+	}
+	sum = first + second;
+	printf("%li\n", sum);
+
+	return (0);
+}
