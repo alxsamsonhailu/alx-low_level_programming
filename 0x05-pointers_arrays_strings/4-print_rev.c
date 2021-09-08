76c72aac8eef594744c50817d1e949a66e4f8d4d
Samson Hailu <alxsamsonhailu@gmail.com>
2021-09-08 12:03:41 +0300

    print

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse
+ * @s: String to be printed
+ */
+void print_rev(char *s)
+{
+	int i;
+	int len;
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
