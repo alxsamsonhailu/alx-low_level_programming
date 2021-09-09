a46a803b4abb78786fb8ddd91db19500303e7532
Samson Hailu <alxsamsonhailu@gmail.com>
2021-09-09 10:58:02 +0300

    rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses an array of integers
+ * @a: Array of integers
+ * @n: Length of the array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int aux;
+	int middle;
+	middle = n / 2;
+	for (i = 0; i < middle; i++)
+	{
+		aux = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = aux;
+	}
+}
