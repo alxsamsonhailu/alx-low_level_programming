09ec408cd8c6be2c84ed4cc26a531abcc8d650dd
Samson Hailu <alxsamsonhailu@gmail.com>
2021-09-15 19:06:40 +0300

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,67 +1,40 @@
 #include "main.h"
-#include <stdio.h>
-
-int brute_force_sqrt(int, int);
-int newton_sqrt(int, int);
 
 /**
- * _sqrt_recursion - Calculates the power x raised to y
- * @n: Number to calculate the square root of
- *
- * Return: Natural square root of n
+ * check_int - checks integer as candidate for square root
+ * @square: square integer
+ * @x: integer
+ * Return: 1 if
  */
+
+int check_int(int square, int x)
+{
+	if (x * x == square)
+		return (x);
+	else if (x > square / x)
+		return (check_int(square, x - 1));
+	else if (x < square / x)
+		return (check_int(square, x + 1));
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ * Return: integer - square root, if none returns -1
+ */
+
 int _sqrt_recursion(int n)
 {
+	int start;
+
+	start = 1;
+
 	if (n < 0)
 		return (-1);
-	if (n <= 1)
-		return (n);
-
-	return (brute_force_sqrt(n, 2));
-}
-
-/**
- * brute_force_sqrt - Calculates the sqrt root of n based on Newton's rule
- * @n: Num to calculate the sqrt root
- * @factor: Factor to be tested as sqrt root
- *
- * Return: Natural sqrt root of n
- */
-int brute_force_sqrt(int n, int factor)
-{
-	if (factor * factor < n)
-		return (brute_force_sqrt(n, factor + 1));
-
-	if (factor * factor == n)
-		return (factor);
-
-	return (-1);
-}
-
-/**
- * newton_sqrt - Calculates the sqrt root of n based on Newton's rule
- * @n: Num to calculate the sqrt root
- * @x: Guess of sqrt root
- *
- * Return: Natural sqrt root of n
- */
-int newton_sqrt(int n, int x)
-{
-	int root = (x + (n / x)) / 2;
-	/*
-	 * This method needs to handle overflow, that's why the cast is needed
-	 */
-	unsigned long root = (x + (n / x)) / 2;
-
-	if (root * root > (unsigned long)n)
-		return (newton_sqrt(n, root));
-
-	if (root * root == (unsigned long)n)
-		return ((int)root);
-
-	/*
-	 * If the root^2 is less than n means that n
-	 * doesn't have natural square root
-	 */
-	return (-1);
+	else if (n == 1)
+		return (1);
+	else
+		return (check_int(n, start));
 }
