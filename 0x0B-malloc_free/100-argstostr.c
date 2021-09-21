a8a523b1f95ef0b743da622ff72b5b139561a8af
Samson Hailu <alxsamsonhailu@gmail.com>
2021-09-21 15:31:45 +0300

    Argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,95 +1,87 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-int get_strings_count(char *s);
-char *get_string(char *s, int *index);
-int get_len(char *s);
+
 /**
- * strtow - Splits a string into words
- * @str: String to be splitted
+ * number - function to calculate number of words
+ * @str: string being passed to check for words
  *
- * Return: Pointer to an array of strings
+ * Return: number of words
+ */
+int number(char *str)
+{
+	int a, num = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (*str == ' ')
+			str++;
+		else
+		{
+			for (; str[a] != ' ' && str[a] != '\0'; a++)
+				str++;
+			num++;
+		}
+	}
+	return (num);
+}
+/**
+ * free_everything - frees the memory
+ * @string: pointer values being passed for freeing
+ * @i: counter
+ */
+void free_everything(char **string, int i)
+{
+	for (; i > 0;)
+		free(string[--i]);
+	free(string);
+}
+
+/**
+ * strtow - function that splits string into words
+ * @str: string being passed
+ * Return: null if string is empty or null or function fails
  */
 char **strtow(char *str)
 {
-	int i, index = 0;
-	int arr_len = 0;
-	char **strings;
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	arr_len =	get_strings_count(str);
-	/* 1 extra space for NULL at the end of array*/
-	strings = malloc((arr_len + 1) * sizeof(char *));
+	int total_words = 0, b = 0, c = 0, length = 0;
+	char **words, *found_word;
 
-	if (strings == NULL || arr_len == 0)
+	if (str == 0 || *str == 0)
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
+	total_words = number(str);
+	if (total_words == 0)
+		return (NULL);
+	words = malloc((total_words + 1) * sizeof(char *));
+	if (words == 0)
+		return (NULL);
+	for (; *str != '\0' &&  b < total_words;)
 	{
-		if (str[i] != ' ')
+		if (*str == ' ')
+			str++;
+		else
 		{
-			strings[index] = get_string(str + i, &i);
-			if (strings[index] == NULL)
+			found_word = str;
+			for (; *str != ' ' && *str != '\0';)
+			{
+				length++;
+				str++;
+			}
+			words[b] = malloc((length + 1) * sizeof(char));
+			if (words[b] == 0)
+			{
+				free_everything(words, b);
 				return (NULL);
-			index++;
+			}
+			while (*found_word != ' ' && *found_word != '\0')
+			{
+				words[b][c] = *found_word;
+				found_word++;
+				c++;
+			}
+			words[b][c] = '\0';
+			b++; c = 0; length = 0; str++;
 		}
 	}
-	strings[arr_len] = NULL;
-	return (strings);
-}
-/**
- * get_strings_count - Calculates the number of individual strings inside s
- * @s: String containing individual strings
- *
- * Return: Quantity of individual strings inside s
- */
-int get_strings_count(char *s)
-{
-	int i, len = 0;
-	int counting_chars;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == ' ')
-			counting_chars = 0;
-		else if (counting_chars == 0)
-		{
-			len++;
-			counting_chars = 1;
-		}
-	}
-	return (len);
-}
-/**
- * get_string - Puts togheter a string of chars
- * @s: String from wich to take chars
- * @index: Index at wich to start taking the chars
- *
- * Return: A string of chars
- */
-char *get_string(char *s, int *index)
-{
-	int i;
-	int len = get_len(s);
-	char *substr = malloc((len + 1) * sizeof(char));
-	if (substr == NULL)
-		return (NULL);
-	for (i = 0; i < len; i++)
-		substr[i] = s[i];
-	/* To start from the last index substring in the higher loop*/
-	*index += len - 1;
-	substr[len] = '\0';
-	return (substr);
-}
-/**
- * get_len - Calculates the length of a string
- * at the point it encounters a space
- * @s: String from which to calculate the length
- *
- * Return: Length of char sequence without space
- */
-int get_len(char *s)
-{
-	int len = 0;
-	while (s[len] != '\0' && s[len] != ' ')
-		len++;
-	return (len);
+	return (words);
 }
