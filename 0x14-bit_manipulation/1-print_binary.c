8b3d6110cac070a7cb50c1dcc5ba4293888b6a1f
Samson Hailu <alxsamsonhailu@gmail.com>
2021-10-14 21:34:27 +0300

    Print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints a binary representation of a number
+ * @n: Number to be represented as binary
+ */
+void print_binary(unsigned long int n)
+{
+	int most_sig_bit_passed = 0;
+	int idx, bits_n = sizeof(n) * 8;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (idx = bits_n - 1; idx >= 0; idx--)
+	{
+		if ((n >> idx) & 1)
+			most_sig_bit_passed = 1;
+		if (most_sig_bit_passed)
+			_putchar('0' + ((n >> idx) & 1));
+	}
+}
